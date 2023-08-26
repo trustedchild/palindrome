c851e7658e622efb5de919d57e046befad75fad1
admin <tguwor@gmail.com>
2023-08-26 18:38:07 -0400

    init npm and refactor palindrome code in index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-// Reverses a string.
-function reverse(string) {
-    return Array.from(string).reverse().join("")
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
   }
-  
-  // Returns true for a palindrome, false otherwise.
-  function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
   }
+}
